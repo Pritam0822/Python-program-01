5ef972f57965fac3c6e4b5a5f7ac8785536d172f
PRITAM DEKA <pritamdeka146@gmail.com>
2026-02-10 19:02:29 +0530

    Add files via upload

---
+++ automation.py
@@ -0,0 +1,4 @@
+import pyttsx3
+engine = pyttsx3.init()
+engine.say("Hello, Kookie! I am a personal assistant created by Pritam. I am here to help you. How can I assist you today?")
+engine.runAndWait()
